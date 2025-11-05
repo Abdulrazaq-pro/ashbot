88b99a6fc560d2f0602579b6f3cd80fed97e047f
abdulrazaq <abdulrazmail@gmail.com>
2025-11-05 07:16:54 +0100

    adjusted the dark toggle

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -63,7 +63,6 @@ const featureList: FeatureProps[] = [
   },
 ];
 
-
 export const Navbar = () => {
   const [isOpen, setIsOpen] = React.useState(false);
   return (
@@ -97,8 +96,14 @@ export const Navbar = () => {
               <SheetHeader className="mb-4 ml-4">
                 <SheetTitle className="flex items-center">
                   <Link href="/" className="flex items-center">
-                    <ChevronsDown className="bg-gradient-to-tr border-secondary from-primary via-primary/70 to-primary rounded-lg w-9 h-9 mr-2 border text-white" />
-                    Shadcn
+                    <Image
+                      src="/logo.png"
+                      alt="Logo"
+                      width={52}
+                      height={52}
+                      className="rounded-lg mr-2 border border-secondary"
+                    />
+                    
                   </Link>
                 </SheetTitle>
               </SheetHeader>
@@ -121,7 +126,7 @@ export const Navbar = () => {
             <SheetFooter className="flex-col sm:flex-col justify-start items-start">
               <Separator className="mb-2" />
 
-              <ToggleTheme />
+              {/* <ToggleTheme /> */}
             </SheetFooter>
           </SheetContent>
         </Sheet>
@@ -173,7 +178,7 @@ export const Navbar = () => {
           </NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
-{/* 
+      {/* 
       <div className="hidden lg:flex">
         <ToggleTheme />
 
